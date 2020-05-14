6d4b4e5d229182dacf57147ee0ad21aaa14b1999
David Afonso <davafons@gmail.com>
2020-05-14 02:28:35 +0100

    :sparkles: Create Tensorboard instance

--- dial_tensorboard_nodes/tensorboard_node/tb_instance_node.py
+++ dial_tensorboard_nodes/tensorboard_node/tb_instance_node.py
@@ -1,11 +1,12 @@
 ## vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 import dependency_injector.providers as providers
 from dial_core.node_editor import Node
+from tensorflow.keras.callbacks import Callback
 
-from .tb_instance_widget  import TensorboardInstanceWidgetFactory
+from .tb_instance_widget import TensorboardInstanceWidgetFactory
 
 if TYPE_CHECKING:
     from .tensorboard_instance_widget import TensorboardInstanceWidget
@@ -19,11 +20,11 @@ class TensorboardInstanceNode(Node):
             title="Tensorboard Instance", inner_widget=tensorboard_instance_widget,
         )
 
-        # self.add_output_port("", port_type=dict)
-        # self.outputs["Hyperparameters"].set_generator_function(self.get_hyperparameters)
+        self.add_output_port("Keras Callbacks", port_type=List[Callback])
 
-    # def get_hyperparameters(self):
-    #     return self.inner_widget.get_hyperparameters()
+        self.outputs["Keras Callbacks"].set_generator_function(
+            self.inner_widget.get_callbacks
+        )
 
     def __reduce__(self):
         return (TensorboardInstanceNode, (self.inner_widget,), super().__getstate__())
--- dial_tensorboard_nodes/tensorboard_node/tb_instance_widget.py
+++ dial_tensorboard_nodes/tensorboard_node/tb_instance_widget.py
@@ -1,15 +1,111 @@
 # vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
 
 import dependency_injector.providers as providers
+from typing import List
+from dial_core.utils import log
 from PySide2.QtCore import QSize
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import (
+    QFileDialog,
+    QFormLayout,
+    QGroupBox,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+from tensorboard import program
+from tensorflow.keras.callbacks import Callback
+
+LOGGER = log.get_logger(__name__)
 
 
 class TensorboardInstanceWidget(QWidget):
+    tensorboard_status = "Stopped"
+    tensorboard_url = ""
+
     def __init__(self, parent: "QWidget" = None):
         super().__init__(parent)
 
+        self._status_label = QLabel(self.tensorboard_status)
+        self._url_label = QLabel(self.tensorboard_url)
+
+        self._logs_path_textbox = QLineEdit()
+        self._logs_path_textbox.setPlaceholderText("Directory to store log files...")
+        self._logs_path_button = QPushButton("Select...")
+        self._logs_path_button.clicked.connect(self._pick_logs_directory)
+
+        self._logs_path_layout = QHBoxLayout()
+        self._logs_path_layout.addWidget(self._logs_path_textbox)
+        self._logs_path_layout.addWidget(self._logs_path_button)
+
+        self._properties_layout = QFormLayout()
+        self._properties_layout.addRow("Status:", self._status_label)
+        self._properties_layout.addRow("Url:", self._url_label)
+        self._properties_layout.addRow("Logs Dir:", self._logs_path_layout)
+
+        self._properties_group = QGroupBox("Properties")
+        self._properties_group.setLayout(self._properties_layout)
+
+        self._launch_button = QPushButton("Launch")
+        self._launch_button.clicked.connect(self.launch_tensorboard)
+
+        self._main_layout = QVBoxLayout()
+        self._main_layout.addWidget(self._properties_group)
+        self._main_layout.addWidget(self._launch_button)
+
+        self.setLayout(self._main_layout)
+
+        self._update_states()
+
+    def get_callbacks(self) -> List[Callback]:
+        return []
+
+    def launch_tensorboard(self):
+        if self.tensorboard_status == "Running":
+            LOGGER.info("Tensosboard is already Running.")
+            self._update_states()
+            return
+
+        if not self._logs_path_textbox.text():
+            LOGGER.warning("No Log Directory selected! Can't run Tensorboard.")
+            return
+
+        self.tensorboard_status = "Running"
+
+        LOGGER.debug("Launching Tensorboard...")
+
+        tb = program.TensorBoard()
+
+        tb.configure(argv=[None, "--logdir", self._logs_path_textbox.text()])
+
+        self.tensorboard_url = tb.launch()
+
+        LOGGER.debug("Tensorboard launched!")
+
+        self._update_states()
+
+    def _pick_logs_directory(self):
+        dir_path = QFileDialog.getExistingDirectory(self, "Logs directory")
+
+        if dir_path:
+            self._logs_path_textbox.setText(dir_path)
+        else:
+            LOGGER.info("Picking Log directory cancelled.")
+
+    def _update_states(self):
+        if self.tensorboard_status == "Stopped":
+            self._launch_button.setEnabled(True)
+
+        if self.tensorboard_status == "Running":
+            self._launch_button.setEnabled(False)
+
+        self._status_label.setText(self.tensorboard_status)
+        self._url_label.setText(self.tensorboard_url)
+
     def sizeHint(self) -> "QSize":
-        return QSize(200, 200)
+        return QSize(400, 200)
+
 
 TensorboardInstanceWidgetFactory = providers.Factory(TensorboardInstanceWidget)
